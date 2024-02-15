b2e46a2f172c2abd779aaa2ecd9be812d3b0f9d7
Evaldo <evaldo@parser.com.br>
2024-02-15 13:37:51 -0300

    Substituição do pacote uni_link para uni_link2

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=E:\environment\flutter3"
-export "FLUTTER_APPLICATION_PATH=E:\Projetos\strava_flutter\example"
+export "FLUTTER_ROOT=/home/evaldo/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/evaldo/projetos/strava_flutter/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.1"
 export "FLUTTER_BUILD_NUMBER=5"
